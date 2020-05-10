5f33e38f9a2ffb79c19a9129ddf438043cc293be
Shubhankar-Gambhir <58947326+Shubhankar-Gambhir@users.noreply.github.com>
2020-05-10 23:52:13 +0530

    Update generate_signature.js

--- assignment2/generate_signature.js
+++ assignment2/generate_signature.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 const prompt = require('prompt-sync')({sigint: true});
 const privateKey = fs.readFileSync('./Key_Pair/private.pem',encoding = 'utf8');
 const data = prompt('enter your data : ');
+const signature_file = prompt('enter signature_file path: ')
 const sign = crypto.createSign('SHA256')
 sign.update(Buffer.from(data))
 signature = sign.sign({key:privateKey, padding:crypto.constants.RSA_PKCS1_PSS_PADDING}).toString('base64')
 console.log('Signature: ',signature);
-fs.writeFile('./false.txt', signature, function (err) {
+fs.writeFile(signature_file, signature, function (err) {
     if (err) return console.log(err);
   });
